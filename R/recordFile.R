b75991d46fb21af914adb8ad9ab9963b8a9a211f
Gabe Becker <gmbecker@ucdavis.edu>
2018-06-20 10:46:42 -0700

    add dummy recordFile.R for collate. vbump

---
+++ R/recordFile.R
@@ -0,0 +1,15 @@
+## ##' @importFrom tools md5sum
+## fileHash = function(fil) {
+##     paste0("MD5_",md5sum(fil))
+## }
+    
+
+
+## recordFile = function(object, ingestfun, db = defaultTDB, resultURI = "",
+##                       code = histry::histry(), force = FALSE,
+##                       verbose = FALSE, symorpos = NULL) {
+    
+    
+
+
+## }
